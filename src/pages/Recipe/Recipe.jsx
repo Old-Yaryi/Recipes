4aa9314ded09667fe4bbfd787c9f37d806918de9
Yaryi <kinomir-v@yandex.ru>
2024-07-31 20:14:23 +0300

    useEffectRecipe

--- src/pages/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe.jsx
@@ -1,29 +1,40 @@
 import { useGetAllIngridients } from "../../Servises/queryIngridients"
 import RecipeItem from "../../Components/RecipeItem/RecipeItem"
 import ingridentsProps from "./recipeMiddleware.js"
+import { useEffect, useState } from "react"
+// import { useState } from "react"
 // import { filterSalut } from "../../Servises/queryIngridients.js";
+
+
+
+
 const Recipe = () => {
 
+
+  const [ingridient, setIngridient] = useState([])
   const { data, isLoading, isSuccess, error } = useGetAllIngridients()
-  // const data = filterSalut("laborum")
+  useEffect(() => {
+    isSuccess ? setIngridient(data) : []
+  }, [data])
+  console.log(ingridient);
+
   if (error) return <div>{error.message}</div>
   if (isLoading) return <div> Loading...</div>
-  if (isSuccess)
-    // console.log(data);
-    return (
-      <div className="main__grid">
-        <aside className="main__aside"><h2>фильтры</h2></aside>
-        <div className="main__container">
-          <h1>здесь будут рецепты</h1>
 
-          {data.map((item) => (
-            <div key={item.id}>
-              <RecipeItem props={ingridentsProps(item)} />
-            </div>
-          ))}
-        </div>
+  return (
+    <div className="main__grid">
+      <aside className="main__aside"><h2>фильтры</h2></aside>
+      <div className="main__container">
+        <h1>здесь будут рецепты</h1>
+
+        {ingridient?.map((item) => (
+          <div key={item.id}>
+            <RecipeItem props={ingridentsProps(item)} />
+          </div>
+        ))}
       </div>
-    )
+    </div>
+  )
 }
 
 export default Recipe
